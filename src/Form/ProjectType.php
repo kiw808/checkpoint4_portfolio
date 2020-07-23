57a7c922c4853f2c750a27d44187e7b923b44e3e
Pierre Muller <pierremanu.muller@gmail.com>
2020-07-23 14:03:07 +0200

    Project CRUD working

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -3,27 +3,36 @@
 namespace App\Form;
 
 use App\Entity\Client;
+use App\Entity\Project;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ClientType extends AbstractType
+class ProjectType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('name', TextType::class)
+            ->add('date', DateType::class)
+            ->add('url', TextType::class)
             ->add('description', TextareaType::class)
-            ->add('image')
+            ->add('client', EntityType::class, [
+                'class' => Client::class,
+                'choice_label' => 'name',
+            ])
+            ->add('technos')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Client::class,
+            'data_class' => Project::class,
         ]);
     }
 }
