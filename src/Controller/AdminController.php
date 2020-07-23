812bfaffd431b566df739c064b6d6ee6ced411fa
Pierre Muller <pierremanu.muller@gmail.com>
2020-07-23 12:03:27 +0200

    login authenticator OK

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class AdminController
+ * @package App\Controller
+ * @Route("/admin", name="admin_" )
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("", name="dash")
+     */
+    public function index()
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
