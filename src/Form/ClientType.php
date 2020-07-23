cd67839b7f6f9687011b8008405229205cc50101
Pierre Muller <pierremanu.muller@gmail.com>
2020-07-23 13:42:46 +0200

    Form OK

--- src/Form/ClientType.php
+++ src/Form/ClientType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Client;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,8 @@ class ClientType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
             ->add('image')
         ;
     }
